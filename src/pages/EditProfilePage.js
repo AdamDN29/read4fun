9b331909b106534163a6a2127e3600d02076eaef
muhrafiq <muhammadr4fiq@gmail.com>
2022-10-27 19:19:47 +0700

    Update Login Page

--- src/pages/EditProfilePage.js
+++ src/pages/EditProfilePage.js
@@ -1,28 +1,80 @@
 import React from 'react';
 import ImgAsset from '../resources';
-
+import '../css/editprofilepage.css'
 import Navbars from '../components/Navbars'
 import Footer from '../components/Footer'
 
 //import component Bootstrap React
-import { Card, Container, Row, Col , Button } from 'react-bootstrap'
+import { Card, Container, Row, Col , Button, Form, InputGroup } from 'react-bootstrap'
 
 function EditProfilePage() {
     return (
         <div>
         <Navbars />   
         <Container className="mt-3">
-            <Row>
-                <Col md="{60}">
-                    <Card className="border-0 rounded shadow-sm">
-                        <Card.Body className="p-4">
-                        <h1>EditProfilePage</h1>
-                        <p class="lead">Test EditProfilePage <strong>Read4Fun</strong></p>
-                        
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
+        <Card className="edit_detail card mx-1">
+                <Card.Body className="edit_detail_body card-body">
+                    <Row>
+                        <Col md='10'>
+                        <Card.Title className="edit_detail_title card-title">Edit Profile</Card.Title>
+                        </Col>
+                        <Col md='2'>
+                        <button>Change Password</button>
+                        </Col>
+                    </Row>
+                        <Row>
+                            {/* Upload Cover */}
+                            <Col xs={3} className="upload_col">
+                               
+                                <img
+                                    
+                                    src={ImgAsset.avatar2}
+                                    alt="Photo Profil"
+                                    height="225px"
+                                />
+                                <InputGroup size="md" className="upload_form mb-3">
+                                    <InputGroup.Text id="basic-addon1" className='icon_upload'>
+                                        <img
+                                            src = {ImgAsset.icon_upload}
+                                        />
+                                    </InputGroup.Text>
+                                    <Form.Control
+                                    placeholder="Enter Link of Story Cover"
+                                    aria-label="Username"
+                                    aria-describedby="basic-addon1"
+                                    />
+                                    <Form.Text id="passwordHelpBlock" muted>
+                                        Please enter a valid link of your cover story
+                                    </Form.Text>
+                                    {/* <InputGroup.Text id="basic-addon2"><Button className='btn_search2'>Search</Button></InputGroup.Text> */}
+                                </InputGroup>
+                            </Col>
+
+                            {/* Edit Detail */}
+                            <Col>
+                                <Form className='form_detail'>
+                                    {/* Story Title Form */}
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <Form.Label className="label_form">Story Title</Form.Label>
+                                        <Form.Control type="email" placeholder="Enter Story Title" />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <Form.Label className="label_form">Story Title</Form.Label>
+                                        <Form.Control type="email" placeholder="Enter Story Title" /> 
+                                    </Form.Group>
+                                    <Button href='/userstory' variant="primary" className="btn_back" >
+                                        Back
+                                    </Button>
+                                    <Button variant="primary"  className="btn_save" type="submit">
+                                        Save Detail
+                                    </Button>
+                                    
+                                </Form>
+                            </Col>
+
+                        </Row>
+                </Card.Body>
+            </Card>
             
         </Container>
         <Footer />   
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,87 @@
-import React from 'react';
+import React, {useState} from 'react'
+import ImgAsset from '../resources'
+import { useNavigate } from 'react-router-dom'
+import axios from "axios";
+import '../css/login.css'
 
 //import component Bootstrap React
-import { Card, Container, Row, Col , Button } from 'react-bootstrap'
+import { Card, Container, Row, Col , Button, Form } from 'react-bootstrap'
 
-function Login() {
+function Login(){
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const loginHandler = async (e) => {
+        e.preventDefault();
+        
+        const formData = new FormData();
+
+        formData.append('emailOrUsername', username);
+        formData.append('password', password);
+
+        await axios.post('https://read4fun-backend.herokuapp.com/api/auth/login', formData)
+        .then((response) => {
+            console.log(response);
+            sessionStorage.setItem("token", response.data.token);
+            sessionStorage.setItem("id", response.data.id);
+            sessionStorage.setItem("user", response.data.name);
+            window.location.href = "/homepage";
+        })
+        .catch((error) => {
+            console.log("ERROR: ", error);
+        })
+    };
+
     return (
-        <Container className="mt-3">
-            <Row>
-                <Col md="{60}">
-                    <Card className="border-0 rounded shadow-sm">
-                        <Card.Body className="p-4">
-                        <h1>Login</h1>
-                        <p class="lead">Test Login<strong>Read4Fun</strong></p>
-                        
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
+        <Container>
+            <div className="row vh-100 align-items-center">
+            <a href="/homepage">
+                <img src = {ImgAsset.logo}
+                className="logo"
+                alt="Read4Fun logo"
+                />
+            </a>
             
+                <div className="col-md-7 forms">
+                    <h1>SIGN IN TO YOUR ACCOUNT !</h1>
+                    <p>You must login to comment and write stories</p>
+                </div>
+                <div className="col-md-5 forms">
+                    <form onSubmit={loginHandler}>
+                        <div className="card-body">
+                            <div className="form-group mb-3">
+                                <input 
+                                type="text" 
+                                className="form-control" 
+                                value={username}
+                                id="username"
+                                onChange={(e) => setUsername(e.target.value)}
+                                placeholder="Email/Username">
+                                </input>
+                            </div>
+                            <div className="form-group mb-3">
+                                <input 
+                                type="password" 
+                                className="form-control" 
+                                value={password} 
+                                id="password" 
+                                onChange={(e) => setPassword(e.target.value)}
+                                placeholder="Password">
+                                </input>
+                            </div>
+                            <button type="submit" className="btn btn-black">
+                                <span>Login</span>
+                            </button>
+                            <div>
+                            <p className="login">Don't have an account? <a href="/register">Register</a></p>
+                            </div>
+                        </div>
+                    </form>
+                    </div>
+            </div>
         </Container>
     );
-}
+};
 
 export default Login;
